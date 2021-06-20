fb88c978626d5591cbf2648ae1a0d08afc04dfd5
Sophia Peckner <31379599+sophiapeckner@users.noreply.github.com>
2021-06-20 01:09:30 -0700

    Almost finished

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-var lyrics = "I thought that I've been hurt before But no one's ever left me quite this sore Your words cut deeper than a knife Now I need someone to breathe me back to life Got a feeling that I'm going under But I know that I'll make it out alive If I quit calling you my lover"
+// var lyrics = "I thought that I've been hurt before But no one's ever left me quite this sore Your words cut deeper than a knife Now I need someone to breathe me back to life Got a feeling that I'm going under But I know that I'll make it out alive If I quit calling you my lover"
+var vals = {}
 const PROMPT = document.getElementById("prompt")
 const INPUT_BOX = document.getElementById("input")
 const TIMER = document.getElementById("timer")
+const IFRAME = document.getElementById("songPlayer")
 
-INPUT_BOX.addEventListener("input", checkInput);
+INPUT_BOX.addEventListener("input", checkInput)
+
+function getSongDetails(){
+    let dict = getRandomSong()
+    myLyric = dict.first
+    myLink = dict.second
+    vals = {
+        first: myLyric,
+        second: myLink,
+    }
+}
 
 // https://stackoverflow.com/questions/56177402/split-each-letter-in-a-div-into-its-own-span
 function newPrompt(){
+    getSongDetails()
+    lyrics = vals.first
     let text = lyrics.split("")  // Returns an array of characters in lyrics
-    let result = "";
+    let result = ""
     PROMPT.innerHTML = ""
 
     text.forEach(char => {
@@ -23,35 +37,51 @@ function checkInput() {
     let correctCharacters = PROMPT.querySelectorAll("span") // Characters in lyrics
     myCharacters.forEach((char, i) => {
         let correctChar = correctCharacters[i]
-        correctChar.classList.remove("correct");
-        correctChar.classList.remove("incorrect");
+        correctChar.classList.remove("correct")
+        correctChar.classList.remove("incorrect")
         if (char == correctChar.innerText){
-            correctChar.classList.add("correct");
+            correctChar.classList.add("correct")
         } else {
-            correctChar.classList.add("incorrect");
+            correctChar.classList.add("incorrect")
         }
     })
 }
 
 // https://stackoverflow.com/questions/31559469/how-to-create-a-simple-javascript-timer
 function startTimer(){
-    let sec = 30
+    let sec = 5
     var timer = setInterval(function(){
         TIMER.innerHTML = sec
         sec--
         if (sec < 0){
-            clearInterval(timer);
-            calculateWPM()
+            clearInterval(timer)
+            end()
         }
     }, 1000)
 }
 
 function calculateWPM(){
-    let correctCharArray = PROMPT.getElementsByClassName("correct");
+    let correctCharArray = PROMPT.getElementsByClassName("correct")
     let numCorrect = correctCharArray.length
     let WPM = Math.round((numCorrect / 5) / 0.5)
-    return WPM
+    document.getElementById("myWPM").innerHTML = "WPM: " + WPM
 }
 
-newPrompt()
-startTimer()
+function end(){
+    IFRAME.style.display = "block"
+    calculateWPM()
+    document.getElementById("input").disabled = true
+    IFRAME.src = vals.second
+}
+
+function reset(){
+    PROMPT.innerHTML = ""
+    INPUT_BOX.innerHTML = ""
+    IFRAME.style.display = "none"
+    document.getElementById("input").disabled = false
+    getSongDetails()
+    newPrompt()
+}
+
+reset()
+// newPrompt()
