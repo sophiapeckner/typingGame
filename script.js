dbd05ef07922f472b46c1e4f2fa3fa463c65136d
Sophia Peckner <31379599+sophiapeckner@users.noreply.github.com>
2021-06-19 19:38:27 -0700

    Main functionality works

--- script.js
+++ script.js
@@ -1,49 +1,57 @@
-const wordBox = document.getElementById("prompt");
-const inputBox = document.getElementById("typeHere");
+var lyrics = "I thought that I've been hurt before But no one's ever left me quite this sore Your words cut deeper than a knife Now I need someone to breathe me back to life Got a feeling that I'm going under But I know that I'll make it out alive If I quit calling you my lover"
+const PROMPT = document.getElementById("prompt")
+const INPUT_BOX = document.getElementById("input")
+const TIMER = document.getElementById("timer")
 
-function isTextAreaOverflow(){
-    // Returns True if text area is overflowing
-    console.log(wordBox.scrollHeight + ">" + wordBox.clientHeight);
-    return wordBox.scrollHeight > wordBox.clientHeight;
+INPUT_BOX.addEventListener("input", checkInput);
+
+// https://stackoverflow.com/questions/56177402/split-each-letter-in-a-div-into-its-own-span
+function newPrompt(){
+    let text = lyrics.split("")  // Returns an array of characters in lyrics
+    let result = "";
+    PROMPT.innerHTML = ""
+
+    text.forEach(char => {
+        result += "<span>" + char + "</span>"
+    })
+    PROMPT.innerHTML = result
+    INPUT_BOX.value = null      // Words not typed yet
 }
 
-function splitToCharacters(word){
-    let l = [];
-    for (var i = 0; i < word.length - 1; i++){
-        l.push(word[i]);    // Pushes letters to l
-    }
-    return l;
-}
-
-function appendToTextArea(){
-    let lastWord;
-    let totalCharacters = [];
-    let flag = true;
-    while (flag){
-        let word = randomWord() + " ";
-        wordBox.value += word;
-        console.log(wordBox.value);
-        if (isTextAreaOverflow()){
-            lastWord = word;
-            flag = false;
-            // console.log("here, lastWord: ", lastWord);
-        } else {    // Only push the words BEFORE overflow
-            let myCharacters = splitToCharacters(word);
-            totalCharacters.push(myCharacters);
+function checkInput() {
+    let myCharacters = INPUT_BOX.value.split("")    // Inputted characters
+    let correctCharacters = PROMPT.querySelectorAll("span") // Characters in lyrics
+    myCharacters.forEach((char, i) => {
+        let correctChar = correctCharacters[i]
+        correctChar.classList.remove("correct");
+        correctChar.classList.remove("incorrect");
+        if (char == correctChar.innerText){
+            correctChar.classList.add("correct");
+        } else {
+            correctChar.classList.add("incorrect");
         }
-    }
-    console.log("Last word: ", lastWord)
-    wordBox.value = wordBox.value.replace(lastWord, "");
-    // return totalCharacters;
+    })
 }
 
-function checkInputFocus(){
-    console.log(inputBox.value)
+// https://stackoverflow.com/questions/31559469/how-to-create-a-simple-javascript-timer
+function startTimer(){
+    let sec = 30
+    var timer = setInterval(function(){
+        TIMER.innerHTML = sec
+        sec--
+        if (sec < 0){
+            clearInterval(timer);
+            calculateWPM()
+        }
+    }, 1000)
 }
 
-function main(){
-    appendToTextArea();
-    checkInputFocus();
+function calculateWPM(){
+    let correctCharArray = PROMPT.getElementsByClassName("correct");
+    let numCorrect = correctCharArray.length
+    let WPM = Math.round((numCorrect / 5) / 0.5)
+    return WPM
 }
 
-main();
+newPrompt()
+startTimer()
